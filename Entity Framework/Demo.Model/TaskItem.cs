932dd2848dbdf56d5d0b6d1a801a1bdd7385b431
santypr <santypr@gmail.com>
2015-07-26 02:48:26 +0100

    Demo de entity framework

---
+++ Entity Framework/Demo.Model/TaskItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Model
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class TaskItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="Título")]
+        public string Title { get; set; }
+        [Display(Name = "Descripción")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        [Display(Name = "Prioridad")]
+        public string Priority { get; set; }
+    }
+}
---
+++ Entity Framework/src/Demo.Web/Controllers/TaskItemController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace vNextADDConnect.Web.Controllers
+{
+    public class TaskItemController1 : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
